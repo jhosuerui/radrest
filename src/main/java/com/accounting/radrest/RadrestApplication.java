716e2debfb12a1c54bc65a40be4bddaf6085e70c
Jhosue Rui <ingrui@gmail.com>
2026-05-28 15:42:02 -0400

    first commit

---
+++ src/main/java/com/accounting/radrest/RadrestApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.radrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accounting/radrest/RadrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.radrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
